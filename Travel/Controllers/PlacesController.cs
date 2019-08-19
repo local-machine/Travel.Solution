f1002e2158ceff74aac919f6751b06c8c6076a4d
Jessica Munoz and Maryana Antonyuk <maryana@>
2019-08-19 16:21:28 -0700

    Edited placescontroller.cs and tested calls

--- Travel/Controllers/PlacesController.cs
+++ Travel/Controllers/PlacesController.cs
@@ -13,28 +13,28 @@ namespace Travel.Controllers
     {
         private TravelContext _db = new TravelContext();
 
-        // GET api/users
+        // GET api/places
         [HttpGet]
         public ActionResult<IEnumerable<Place>> Get()
         {
             return _db.Places.ToList();
         }
 
-        // POST api/values
+        // POST api/places
         [HttpPost]
         public void Post([FromBody] Place place)
         {
             _db.Places.Add(place);
             _db.SaveChanges();
         }
-        // GET api/users/5
+        // GET api/places/5
         [HttpGet("{id}")]
         public ActionResult<Place> Get(int id)
         {
             return _db.Places.FirstOrDefault(x => x.PlaceId == id);
         }
 
-        // PUT api/users/5
+        // PUT api/places/5
         [HttpPut("{id}")] 
         public void Put(int id, [FromBody] Place place)
         {
@@ -43,7 +43,7 @@ namespace Travel.Controllers
             _db.SaveChanges();
         }
 
-        // DELETE api/users
+        // DELETE api/places
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
@@ -52,16 +52,17 @@ namespace Travel.Controllers
             _db.SaveChanges();
         }
          // GET api/places/city
-        [HttpGet("{city}")]
+        [HttpGet("city/{city}")]
         public ActionResult<Place> Get(string city)
         {
             return _db.Places.FirstOrDefault(x => x.City == city);
         }
-        // // GET api/places/country
-        // [HttpGet("{country}")]
-        // public ActionResult<Place> Get(string country)
-        // {
-        //     return _db.Places.FirstOrDefault(x => x.Country == country);
-        // }
+        
+         // GET api/places/country
+        [HttpGet("country/{country}")]
+        public ActionResult<Place> GetCountry(string country)
+        {
+            return _db.Places.FirstOrDefault(x => x.Country == country);
+        }
     }
 }
